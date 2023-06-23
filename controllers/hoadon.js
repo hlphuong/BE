1b7d750763ff132db3052d9622cb5b6c0b0232d3
zer0ne1 <nguyenkhanhtung.nkt01@gmail.com>
2023-06-23 07:48:37 +0700

    add donhang

--- controllers/hoadon.js
+++ controllers/hoadon.js
@@ -2,24 +2,44 @@ const db = require('../utils/db')
 
 
 const get = async(req,res) => {
-    const{count,rows} = await db.hoadon.findAndCountAll()
+    const{count,rows} = await db.donhang.findAndCountAll()
     res.set('Content-Range', count).send(rows);
 }
 
-//api getbyID cho hoadon
+//api getbyID cho donhang
 const getById = async (req, res) => {
-    const hoadon = await db.hoadon.findOne({
+    const donhang = await db.donhang.findOne({
         where: {id: req.params.id}
     });
-    if(!hoadon) res.status(404).send('không tìm thấy mặt hàng!');
-    else res.send(hoadon);
+    if(!donhang) res.status(404).send('không tìm thấy mặt hàng!');
+    else res.send(donhang);
 }
+
 const create = async (req, res) => {
-    const hoadon = await db.hoadon.create({
+    const donhang = await db.donhang.create({
         ...req.body
     });
-    res.send(hoadon);
+    if(typeof(req.body.ctdonhangs) === 'object') {
+        const ctdonhang = req.body.ctdonhangs.map(ctdonhang=> {
+            const ct = {
+                'id_donhang': donhang.id,
+                'id_hang': ctdonhang.id_hang,
+                'soluong': ctdonhang.soluong
+            }
+            return ct
+        });
+        db.ctdonhang.bulkCreate(ctdonhang)
+    }
+    else{
+        db.ctdonhang.create({
+            id_hang: req.body.ctdonhangs.id_hang,
+            id_donhang: donhang.id,
+            soluong: req.body.ctdonhangs.soluong
+        })
+    }
+    res.send(donhang)
 }
+
 module.exports = {
     get,
     getById,
